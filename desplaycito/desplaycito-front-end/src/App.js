fdb44b2c54eccf1410c9a8be8055f4e5861f55ee
Stella Ma <stellama98@gmail.com>
2018-09-29 21:33:56 -0400

    Added Playlist Page and Backbutton

--- desplaycito/desplaycito-front-end/src/App.js
+++ desplaycito/desplaycito-front-end/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import playbutton from './PlayButtonLogo.png';
+import backbutton from './backbutton.png';
 
 
 import axios from 'axios';
@@ -9,12 +10,12 @@ import axios from 'axios';
 function PlaylistButton(props){
   return (
     <div className= "playlistbuttons">
-        <button className = "groupbuttons" >
+        <button className = "groupbuttons" onClick={props.clickFunction}>
           <h1 className= "playlisttitles">{props.title1}</h1>
           <p className="description">{props.description1}</p>
         </button>
         <div className="sidedivider"/>
-        <button className = "groupbuttons" >
+        <button className = "groupbuttons" onClick={props.clickFunction}>
           <h1 className= "playlisttitles">{props.title2}</h1>
           <p className="description">{props.description2}</p>
         </button>
@@ -22,17 +23,20 @@ function PlaylistButton(props){
   )
 }
 
-
 class App extends Component {
 
   constructor(){
     super();
     this.state = {
       loggedIn: false,
+      playlistChosen: false,
+      goBack: true,
     };
     this.componentDidMount = this.componentDidMount.bind(this);
     this.authorizeMySpotify = this.authorizeMySpotify.bind(this);
     this.checkLogIn = this.checkLogIn.bind(this);
+    this.checkplaylistChosen = this.checkplaylistChosen.bind(this);
+    this.goBackToDashboard = this.goBackToDashboard.bind(this);
   }
 
   componentDidMount(){
@@ -41,6 +45,20 @@ class App extends Component {
       this.setState({
         logInUrl: res.data.url
       });
+    })
+  }
+
+  checkplaylistChosen(){
+    this.setState({
+      playlistChosen: true,
+      goBack: false
+    });
+  }
+
+  goBackToDashboard(){
+    this.setState({
+      playlistChosen: false,
+      goBack: true
     })
   }
 
@@ -59,25 +77,46 @@ class App extends Component {
     });
   }
 
-
   render() {
     let loggedIn = this.state.loggedIn;
+    let goBack = this.state.goBack;
+    let playlistChosen = this.state.playlistChosen;
 
     let buttonOption =
-    <div className= "connect-section">
-      <button className="spotify-button" onClick={this.authorizeMySpotify}> <b>AUTHORIZE MY SPOTIFY</b></button>
-      <button className="spotify-button" onClick={this.checkLogIn}> <b>CHECK IF LOGGED IN</b></button>
+    <div>
+      <div className="intro-align">
+        <h3 className="intro-text">DesPLAYcito is your personal playlist generator. Connect it with your Spotify to see playlists curated to your tastes!</h3>
+      </div>
+      <div className= "connect-section">
+        <button className="spotify-button" onClick={this.authorizeMySpotify}> <b>AUTHORIZE MY SPOTIFY</b></button>
+        <div className = "sidedivider" />
+        <button className="spotify-button" onClick={this.checkLogIn}> <b>CHECK IF LOGGED IN</b></button>
+      </div>
     </div>
 
-    if (loggedIn){
+    if (loggedIn && goBack){
       var playlist = [["Top Artists", "Playlist of songs by your top artists"],["Top Tracks", "Playlist of your top tracks"], ["Related Artists","Playlist of songs by artists related to your top artists"],["Saved Tracks","Playlist of songs from your saved tracks"]]
 
       buttonOption = [];
 
       for (let i=0; i < 3; i += 2)
-        buttonOption.push(<PlaylistButton title1={playlist[i][0]} description1={playlist[i][1]} title2={playlist[i+1][0]} description2={playlist[i+1][1]}/>);
+        buttonOption.push(<PlaylistButton
+          title1={playlist[i][0]}
+          description1={playlist[i][1]}
+          title2={playlist[i+1][0]}
+          description2={playlist[i+1][1]}
+          clickFunction={this.checkplaylistChosen}
+        />);
     }
 
+    if(playlistChosen){
+      buttonOption =
+      <div>
+      <img id="backbutton" src={backbutton} onClick={this.goBackToDashboard}/>
+        <div>
+        </div>
+      </div>
+    }
 
     return (
       <div className ="color-background">
